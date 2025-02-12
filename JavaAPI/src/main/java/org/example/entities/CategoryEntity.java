3c6cdfdf1ef5e4d5c7a3dbf1e3bb5eeef86619d7
MIRADA <vitaliy.laben@gmail.com>
2025-02-12 15:44:15 +0200

    Add Post Method

--- JavaAPI/src/main/java/org/example/entities/CategoryEntity.java
+++ JavaAPI/src/main/java/org/example/entities/CategoryEntity.java
@@ -22,6 +22,46 @@ public class CategoryEntity {
     @Column(length = 255)
     private String image;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Column(length = 40000)
     private String description;
 }
