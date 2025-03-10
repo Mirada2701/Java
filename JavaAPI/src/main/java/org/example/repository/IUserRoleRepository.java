28a03de37a5641fb2164bd914f745383363bfd99
Mirada2701 <mirada-24@ukr.net>
2025-03-10 15:45:59 +0200

    Start work with JWT

--- JavaAPI/src/main/java/org/example/repository/IUserRoleRepository.java
+++ JavaAPI/src/main/java/org/example/repository/IUserRoleRepository.java
@@ -1,7 +1,12 @@
 package org.example.repository;
 
+import org.example.entities.UserEntity;
 import org.example.entities.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IUserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+    List<UserRoleEntity> findByUser(UserEntity User);
 }
